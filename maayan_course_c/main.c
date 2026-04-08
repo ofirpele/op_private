bc60ca19ad9a4ea2372db5c68414e7599d0865f8
Ofir Pele <ofirpele@gmail.com>
2026-04-08 16:27:00 +0300

    refactoring

--- maayan_course_c/main.c
+++ maayan_course_c/main.c
@@ -2,6 +2,24 @@
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    
+    float fahr, celsius;
+    int lower, upper, step;
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = upper;
+    while (fahr >= lower)
+    {
+        celsius  = (0.5/0.9) * (fahr - 32.0);
+        printf("%3.0f %6.1f\n" , fahr, celsius);
+        fahr = fahr - step;
+    }
+
+    
     return 0;
 }
+    
+
+    
